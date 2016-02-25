4431704476affe2151697c25a61bbd51df328d73
ACDC <ACDC@faforever.com>
2016-02-25 23:44:53 +1100

    Add plots to exmine distribution of predicted probability of outcome

--- plot_progression.py
+++ plot_progression.py
@@ -3,6 +3,7 @@ import pytrueskill as ts
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import scipy.stats
 
 replays = load_replays('data/replaydumper/replays.csv')
 nplayers = 1 + max(np.max(replays.playerid1), np.max(replays.playerid2))
@@ -17,37 +18,81 @@ with open('data/replaydumper/uniquePlayerIds.csv') as fp:
     for row in csvreader:
         playerAliases[int(row[0])] = row
 
+nPlayersExamine = 5
+figs_progression = [ plt.subplots() for n in range(nPlayersExamine) ]
+fig_filenames = { }
+probabilities = [ [] for n in range(nPlayersExamine) ]
+all_probabilities = []
+aliases = [None for n in range(nPlayersExamine)]
+environments = ( np.array([1500., 500., 250., 5., 0.1]),
+                 np.array([1500., 500., 240., 10., 0.045]),
+                 np.array([1500., 500., 240., 18., 0.045]) )
 
-figs = [ plt.subplots() for n in range(5) ]
-fig_filenames = {  }
-for env in ( np.array([1500., 500., 240., 18., 0.045]),
-             np.array([1500., 500., 240., 10., 0.045]),
-             np.array([1500., 500., 250., 5., 0.1]) ):
+for nenv,env in enumerate(environments):
 
     ratings[:,0] = env[0]
     ratings[:,1] = env[1]
     r1,r2,nGames1,nGames2,L = ts.rate_1vs1(env,pids1,pids2,scores12,ratings)
+    all_probabilities += [L]
 
     finalGameCount = np.zeros(nplayers,dtype=np.int)
     finalGameCount[pids1] = nGames1
     iSorNGames = np.argsort(finalGameCount)[::-1]
-    for (f,ax),PID in zip(figs,iSorNGames):
+    for nplayer,((f1,ax1),PID) in enumerate(zip(figs_progression,iSorNGames)):
         idx1 = pids1==PID
         idx2 = pids2==PID
-        alias = ','.join(playerAliases[PID][1:])
+        aliases[nplayer] = ','.join(playerAliases[PID][1:])
 
         mu = idx1*r1[:,0] + idx2*r2[:,0]
         sigma = idx1*r1[:,1] + idx2*r2[:,1]
         mu = mu[idx1 | idx2]
         sigma = sigma[idx1 | idx2]
         l = L[idx1 | idx2]
+        probabilities[nplayer] += [l]
 
-        ax.plot(np.arange(len(mu)),mu-3*sigma,label='mu=%d, sigma=%d, beta=%d, tau=%.1f, pdraw=%.2f, L=%d' % tuple([x for x in env] + [np.sum(np.log(l))]))
-        ax.set_title(alias)
-        ax.legend(loc='lower center')
-        ax.grid(True)
-        fig_filenames[f] = 'rating_progression_%s.png' % (alias)
+        label = 'mu=%d, sigma=%d, beta=%d, tau=%.1f, pdraw=%.2f, L=%d' % tuple([x for x in env] + [np.sum(np.log(l))])
+        ax1.plot(np.arange(len(mu)),mu-3*sigma,label=label)
+        ax1.set_title(aliases[nplayer])
+        ax1.legend(loc='lower center', prop={'size':8})
+        ax1.grid(True)
+        fig_filenames[f1] = 'rating_progression_%s.png' % (aliases[nplayer])
 
-for f,ax in figs:
+for f,ax in figs_progression:
     f.savefig(fig_filenames[f])
     plt.close(f)
+
+for nplayer,(l,alias) in enumerate(zip(probabilities,aliases)):
+    getlabel = lambda n: 'mu=%d, sigma=%d, beta=%d, tau=%.1f, pdraw=%.2f, L=%d' % tuple([x for x in environments[n]] + [np.sum(np.log(l[n]))])
+
+    logL = np.array(l).T
+    fig,ax = plt.subplots()
+    for n in range(len(environments)):
+        ax.plot(np.sort(logL[:,0]), np.sort(logL[:,n]), '-', linewidth=1, label=getlabel(n))
+    ax.set_xlabel('quantile')
+    ax.set_ylabel('quantile')
+    ax.set_title('QQ plots, predicted probability of outcome, %s' % (alias))
+    ax.legend(loc='lower right', prop={'size':8})
+
+    fig.savefig('QQ_predicted_probability_of_outcome_%s.png' % (alias))
+    plt.close(fig)
+
+fig,ax = plt.subplots()
+l = np.array(all_probabilities).T
+logl = np.log(all_probabilities).T
+labels = [ 'mu=%d, sigma=%d, beta=%d, tau=%.1f, pdraw=%.2f, L=%d' % tuple([x for x in env] + [np.sum(logl[:,n])]) for n,env in enumerate(environments) ]
+for n,env in enumerate(environments):
+    ax.plot(np.sort(l[:,0]), np.sort(l[:,n]), '-', linewidth=1, label=labels[n])
+ax.set_xlabel('quantile')
+ax.set_ylabel('quantile')
+ax.set_title('QQ plots, predicted probability of outcome')
+ax.legend(loc='lower right', prop={'size':8})
+fig.savefig('QQ_predicted_probability_of_outcome.png')
+plt.close(fig)
+
+fig,ax = plt.subplots()
+ax.hist(l,bins=100,histtype='step',label=labels)
+ax.set_xlabel('predicted probability of outcome')
+ax.set_ylabel('frequency')
+ax.legend(loc='lower center',prop={'size':8})
+fig.savefig('distribution_predicted_probability_of_outcome.png')
+plt.close(fig)
